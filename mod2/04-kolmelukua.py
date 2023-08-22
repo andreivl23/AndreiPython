d958925d98fa3936665a8acbe74e1e4434218d4d
Andrei <anvlsnko@gmail.com>
2023-08-22 10:19:23 +0300

    Added 04-kolmelukua.py

---
+++ mod2/04-kolmelukua.py
@@ -0,0 +1,8 @@
+ens = int(input("Anna ensimmäinen luku: "))
+toka = int(input("Anna toinen luku: "))
+kolmas = int(input("Anna kolmas luku: "))
+print("Summa:", ens + toka + kolmas)
+print("Tulo:", ens * toka * kolmas)
+tulo = (ens + toka + kolmas) / 3
+pyor = round(tulo,2)
+print("Keskiarvo:", pyor)
