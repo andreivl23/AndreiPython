97f42053f017b7b268866c27c229e984841c9aa5
Andrei <anvlsnko@gmail.com>
2023-08-22 11:01:26 +0300

    Added 05-massa.py

---
+++ mod2/05-massa.py
@@ -0,0 +1,12 @@
+leiviska = float(input("Anna leiviskät: "))
+naula = float(input("Anna naulat: "))
+luoti = float(input("Anna luoti: "))
+
+naulat = leiviska * 20 + naula
+luotit = naulat * 32 + luoti
+grammat = luotit * 13.3
+kilogrammat = grammat // 1000  # double slash gives the whole number
+grammaa = grammat % 1000  # % gives remainder after division
+
+print("Massa on", kilogrammat, "kilogrammaa ja", int(grammaa), "grammaa")
+print("Oikea tulos grammoissa: ", grammat)
