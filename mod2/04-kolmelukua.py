a3b70e96727ecafe4249e9a67b4fb4c7364bdd6f
Andrei <anvlsnko@gmail.com>
2023-08-22 11:40:59 +0300

    Muutettu pyöristys metodi

--- mod2/04-kolmelukua.py
+++ mod2/04-kolmelukua.py
@@ -4,5 +4,5 @@ kolmas = int(input("Anna kolmas luku: "))
 print("Summa:", ens + toka + kolmas)
 print("Tulo:", ens * toka * kolmas)
 tulo = (ens + toka + kolmas) / 3
-pyor = round(tulo, 2)
-print("Keskiarvo:", pyor)
+#pyor = round(tulo, 2)
+print(f"Keskiarvo: {tulo:.2f}")
