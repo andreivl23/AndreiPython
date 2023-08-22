60b88b7ad42a8e7dfd112a36e6403f8726ceb627
Andrei <anvlsnko@gmail.com>
2023-08-22 10:09:44 +0300

    Added 03-suorakulmio.py in mod2

---
+++ mod2/03-suorakulmio.py
@@ -0,0 +1,5 @@
+kanta = int(input("Anna kanta: "))
+korkeus = int(input("Anna korkeus: "))
+piiri = 2 * kanta + 2 * korkeus
+pintaala = kanta * korkeus
+print("Piiri on: ", piiri, "\nPinta-ala on: ", pintaala, sep="")
