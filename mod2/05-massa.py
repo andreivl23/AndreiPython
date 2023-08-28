844efccdb35b53d5d323fa047477ed09136bd247
Andrei <anvlsnko@gmail.com>
2023-08-28 13:26:05 +0300

    Calculations moved to print sentence

--- mod2/05-massa.py
+++ mod2/05-massa.py
@@ -5,8 +5,9 @@ luoti = float(input("Anna luoti: "))
 naulat = leiviska * 20 + naula
 luotit = naulat * 32 + luoti
 grammat = luotit * 13.3
-kilogrammat = grammat // 1000  # double slash gives the whole number
-grammaa = grammat % 1000  # % gives remainder after division
+#kilogrammat = grammat // 1000  # double slash gives the whole number
+#grammaa = grammat % 1000  # % gives remainder after division
 
-print("Massa on", kilogrammat, "kilogrammaa ja", int(grammaa), "grammaa")
+# print("Massa on", kilogrammat, "kilogrammaa ja", int(grammaa), "grammaa")
+print(f"Massa on {grammat // 1000} kilogrammaa ja {grammat % 1000:.0f} grammaa")
 print("Oikea tulos grammoissa: ", grammat)
