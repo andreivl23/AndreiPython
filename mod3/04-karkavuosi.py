2c9baa28b3965e0bf848dcb78eaa854a3625ad9d
Andrei <anvlsnko@gmail.com>
2023-08-28 15:31:27 +0300

    Korjattu: sadalla jaolliset ei enää karkausvuodet

--- mod3/04-karkavuosi.py
+++ mod3/04-karkavuosi.py
@@ -1,6 +1,9 @@
 vuosi = int(input("Vuosi?: "))
 
-if (vuosi % 4) == 0:
-    print("Karkavuosi!")
+if (vuosi % 100) == 0 and (vuosi % 400) == 0:
+    print("Karkausvuosi 100!")
+elif (vuosi % 4) == 0 and (vuosi % 100) != 0:
+    print("Karkausvuosi!")
+
 else:
-    print("Ei karkavuosi")
+    print("Ei karkausvuosi")
