fdb32545c10ad1ca2d831154b3105030a2c2cfed
Andrei <anvlsnko@gmail.com>
2023-08-29 11:13:44 +0300

    pinta-alan yhtälö muuttui

--- mod2/02-ympyrasade.py
+++ mod2/02-ympyrasade.py
@@ -2,6 +2,6 @@ import math  # lisätään math moduuli
 
 pi_arvo = math.pi  # lisätään math moduulista Pin arvo muutujaan
 sade = int(input("Anna ympyrän säde: "))
-tulos = sade*pi_arvo
+tulos = sade ** 2 * pi_arvo
 pyor_numero = round(tulos, 2)  # pyöristetään kahden desimaalin tarkkuuteen.
 print("Ympyrän pinta ala on:", pyor_numero)
