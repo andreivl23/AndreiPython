8e99fdeab1d1c3c8f01b968899662da641286eac
Andrei <anvlsnko@gmail.com>
2023-08-29 13:09:41 +0300

    Poistettu ylimääräinen koodi

--- mod4/06-piinlikiarvo.py
+++ mod4/06-piinlikiarvo.py
@@ -1,14 +1,9 @@
-import math, random
+import random
 
 x = random.uniform(-1, 1)
 y = random.uniform(-1, 1)
-pi_arvo = math.pi
-sade = 1
 N = int(input("Montako kertoja?: "))
 n = 0
-A_pinta_ala = pi_arvo * sade ** 2
-B_pinta_ala = 4 * sade
-#ero = B_pinta_ala / A_pinta_ala
 yhtalo = x ** 2 + y ** 2 < 1
 kertoja = 0
 
