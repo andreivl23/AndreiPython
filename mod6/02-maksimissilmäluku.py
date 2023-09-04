15f035f63dafa224abaffbfe94e25ebe12d3e463
Andrei <anvlsnko@gmail.com>
2023-09-04 14:52:30 +0300

    Added 02-maksimissilmäluku.py

---
+++ mod6/02-maksimissilmäluku.py
@@ -0,0 +1,18 @@
+import random
+def noppa(tahkoja):
+    silmaluku = random.randint(1,tahkoja)
+    return silmaluku
+
+tahkoja = int(input("Tahkoja: "))
+maksimi = int(input("Maksimisilmäluku: "))
+
+
+silmaluku = noppa(tahkoja)
+
+while silmaluku != maksimi:
+
+        print(f"Heitetään... Silmäluku on {silmaluku}")
+        silmaluku = noppa(tahkoja)
+
+
+print(f"No niin saatiin silmaluvuksi {silmaluku}.")
