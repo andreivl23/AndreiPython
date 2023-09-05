e9f45b77783309d6b4fdc5c196e2c69358f5b2b6
Andrei <anvlsnko@gmail.com>
2023-09-05 10:56:20 +0300

    Added main()

--- mod7/01-kuukaudennumero.py
+++ mod7/01-kuukaudennumero.py
@@ -3,15 +3,18 @@ Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron, jonka jälkeen o
 Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
 Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on ensimmäinen talvikuukausi.
 '''
+def main():
+    kuukaudet = ("Tammikuu","Helmikuu","Maliskuu","Huhtikuu","Toukokuu","Kesäkuu","Heinäkuu","Elokuu","Syyskuu","Lokakuu","Marraskuu","Joulukuu")
+    kaudet = ("talvi","kevät","kesä","syksy")
 
-kuukaudet = ("Tammikuu","Helmikuu","Maliskuu","Huhtikuu","Toukokuu","Kesäkuu","Heinäkuu","Elokuu","Syyskuu","Lokakuu","Marraskuu","Joulukuu")
-kaudet = ("talvi","kevät","kesä","syksy")
+    kuukausi = int(input("Anna kuukausi numerona: "))
 
-kuukausi = int(input("Anna kuukausi numerona: "))
+    # If is started only if user gave value between 1 and 12
+    if 1 <= kuukausi <= 12:
+        # % 12 gives 0 as an answer if the month is 12th, and 0 is Winter. // gives floored int number. 5/3 = 1,66 5//3 = 1
+        kausinumero = (kuukausi % 12) // 3
+        kausi = kaudet[kausinumero]
+        print(f"{kuukaudet[kuukausi-1]} on {kausi}")
 
-# If is started only if user gave value between 1 and 12
-if 1 <= kuukausi <= 12:
-    # % 12 gives 0 as an answer if the month is 12th, and 0 is Winter. // gives floored int number. 5/3 = 1,66 5//3 = 1
-    kausinumero = (kuukausi % 12) // 3
-    kausi = kaudet[kausinumero]
-    print(f"{kuukaudet[kuukausi-1]} on {kausi}")
+
+main()
