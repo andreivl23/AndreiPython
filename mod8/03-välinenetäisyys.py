08b99b99a85fab98cc2b74bd07664d8fda783cbe
Andrei <anvlsnko@gmail.com>
2023-09-11 14:34:23 +0300

    Added 03-välinenetäisyys.py

---
+++ mod8/03-välinenetäisyys.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from geopy import distance
+
+
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='123321',
+         autocommit=True
+         )
+def haekoordinatit(icao):
+
+    sql = "SELECT latitude_deg, longitude_deg FROM airport"
+    sql += " WHERE ident='" + icao + "'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+def main():
+
+    icao = input("Anna ICAO: ") #EFHK
+    koordinatit1 = haekoordinatit(icao)
+    icao = input("Anna toinen ICAO: ") #EETN
+    koordinatit2 = haekoordinatit(icao)
+    print(f"Etäisyys näiden lentokentien välillä on {distance.distance(koordinatit1,koordinatit2).km:.0f} kilometria.")
+
+
+main()
