7dd43c68bbba4b777465b9fd5dbfedd973c56637
Andrei <anvlsnko@gmail.com>
2023-10-21 12:17:49 +0300

    added 04-autokilpailu.py

---
+++ mod9/04-autokilpailu.py
@@ -0,0 +1,40 @@
+from random import randint
+
+
+class Auto:
+    def __init__(self, rekkari, huippunopeus):
+        self.rekkari = rekkari
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+    def kiihdyta(self, kiihdytys):
+        self.nopeus += kiihdytys
+        if self.nopeus > self.huippunopeus:
+            self.nopeus = 142
+        if self.nopeus < 0:
+            self.nopeus = 0
+        return
+
+    def kulje(self, tunnit):
+        self.matka += self.nopeus * tunnit
+        return
+
+autot = []
+
+for i in range(11):
+    auto = Auto(f"ABC-{i}", randint(100,200))
+    autot.append(auto)
+
+stop = 0
+
+while stop != 1:
+    for i in range(11):
+        autot[i].kiihdyta(randint(-10,15))
+        autot[i].kulje(1)
+        if autot[i].matka >= 10000:
+            stop = 1
+
+print("Rekkari :: Ajettu etäisyys :: Huippunopeus")
+for i in range(11):
+    print(f"{autot[i].rekkari} ::::::::: {autot[i].matka} km :::: {autot[i].huippunopeus} km/h")
