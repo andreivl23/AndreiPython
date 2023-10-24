3a17ba7b656b483bf37151cefbc3aed897c9437b
Andrei <anvlsnko@gmail.com>
2023-10-24 12:54:08 +0300

    in range(11) --> (10)

--- mod9/04-autokilpailu.py
+++ mod9/04-autokilpailu.py
@@ -29,12 +29,12 @@ for i in range(11):
 stop = 0
 
 while stop != 1:
-    for i in range(11):
+    for i in range(10):
         autot[i].kiihdyta(randint(-10,15))
         autot[i].kulje(1)
         if autot[i].matka >= 10000:
             stop = 1
 
 print("Rekkari :: Ajettu etäisyys :: Huippunopeus")
-for i in range(11):
-    print(f"{autot[i].rekkari} ::::::::: {autot[i].matka} km :::: {autot[i].huippunopeus} km/h")
+for i in range(10):
+    print(f"{autot[i+1].rekkari} ::::::::: {autot[i].matka} km :::: {autot[i].huippunopeus} km/h")
